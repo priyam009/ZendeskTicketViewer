a2f951344011671c03760522aa1d6fe0403e383a
Priyam <priyam009@live.com>
2021-05-15 19:38:46 +1000

    Add different port for test

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 
 //Instantiate Express
 const app = express();
 
 //Set PORT
-const PORT = 3000;
+let PORT = 3000;
+
+if (process.env.NODE_ENV === "test") {
+  PORT = 3001;
+}
 
 //Serve static files
 app.use(express.static(path.join(__dirname + "/public")));
 
 //Parse JSON and URLencoded data using middleware
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Register app routes
